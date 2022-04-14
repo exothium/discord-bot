8463360ec3cce201482c674faae3782785438600
angelobastos <angelo.bastos@feralbyte.com>
2022-04-14 12:21:01 +0100

    added pool connection release for cronJob (memory leak since the pool were still alive each iteration of the cronjob, therefore reaching a connection limit).

--- cronJobs/scanTweets.js
+++ cronJobs/scanTweets.js
@@ -3,9 +3,12 @@ const config = require("../config.json");
 const { Intents, Client } = require('discord.js');
 const { getUser, userLikes, addTweets, addLikes, updateTweets, updateLikes } = require('../twitter_Api/twitter');
 
+const db = require("../twitter_Api/database/connection");
+
 async function scanTweets() {
     await addTweets();
     await addLikes();
+    db.end();
     console.log('tweets scanned and added to DB');
 }
 
